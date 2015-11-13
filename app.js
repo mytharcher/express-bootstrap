30b5200747c2f65df723f6d5079a7563471f4d7a
mytharcher <mytharcher@gmail.com>
2015-11-13 13:14:00 +0800

    Update version.

--- app.js
+++ app.js
@@ -9,40 +9,6 @@ var startTime = new Date();
 
 var app = express();
 
-// 配置http请求处理中间件
-app.configure(function () {
-	// 所有请求记录日志
-	app.use(express.logger());
-	
-	// 启用请求参数丰富形式的解析
-	// http://expressjs.com/api.html#req.body
-	app.use(express.bodyParser());
-	
-	// 启用cookie自动解析
-	app.use(express.cookieParser());
-	
-	// 启用session
-	app.use(express.session({
-		key: config.session.key,
-		secret: startTime.toString(),
-		cookie: {maxAge: config.session.maxAge}
-	}));
-	
-	// 启用gzip压缩
-	app.use(express.compress());
-	
-	// 指明网站静态资源目录
-	app.use(express['static'](__dirname + config.path.assets));
-});
-
-app.configure('development', function () {
-	app.use(express.session({
-		key: config.session.key,
-		secret: startTime.toString(),
-		cookie: {maxAge: 60000}
-	}));
-});
-
 // 路由处理
 rainbow.route(app, config.path);
 
